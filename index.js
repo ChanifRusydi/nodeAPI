eef29f675b41b8800528a52d5addaf07890c0fa2
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-02 07:10:08 +0700

    change res message

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.post('/', (req, res) => res.send('Hello World!'));
-app.put('/', (req, res) => res.send('Hello World!'));
-app.delete('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is HTTP method GET'));
+app.post('/', (req, res) => res.send('This is HTTP method POST'));
+app.put('/', (req, res) => res.send('This is HTTP method PUT'));
+app.delete('/', (req, res) => res.send('This is HTTP method DELETE'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
