1ea562599c7d2427c5dd9b6c05c39560b9c81f49
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-02 08:18:50 +0700

    add parameters.js

---
+++ parameter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/firstnumber/:number1/secondnumber/:number2', (req, res) => {
+    res.send(req.params);
+});
+
+app.get('/:number1/:number2', (req, res) => {
+    res.send(req.params);
+}
+
+app.get('/firstnumber/:number1/secondnumber/:number2/thirdnumber/:number3/fourthnumber/:number4', (req, res) => {
+    result=parseInt(req.params.number1)+parseInt(req.params.number2);
+    res.json({"data":req.params,"result":result});
+}
