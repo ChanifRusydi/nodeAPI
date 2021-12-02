f5e8d8c6930a388dfaa044b87fc205f32b16dc65
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-02 10:02:31 +0700

    fixing parameter.js

--- parameter.js
+++ parameter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+app.get('/', (req, res) => res.send('This is HTTP method GET'));
 app.get('/firstnumber/:number1/secondnumber/:number2', (req, res) => {
     res.send(req.params);
 });
@@ -24,3 +24,9 @@ app.get('/divide/:number/:number2', (req, res) => {  //number2 is optional
     result=parseInt(req.params.number1)/parseInt(req.params.number2);
     res.json({"data":req.params,"result":result});
 });
+app.get('/multiply/:number1/:number2', (req, res) => {
+    result=parseInt(req.params.number1)*parseInt(req.params.number2);
+    res.json({"data":req.params,"result":result});
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
