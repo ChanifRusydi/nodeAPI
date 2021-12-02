093ad554927f997888a13aff99338609fe5d9f1d
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2021-12-02 08:21:15 +0700

    fixing parameter.js

--- parameter.js
+++ parameter.js
@@ -9,7 +9,7 @@ app.get('/firstnumber/:number1/secondnumber/:number2', (req, res) => {
 
 app.get('/:number1/:number2', (req, res) => {
     res.send(req.params);
-}
+});
 
 app.get('/firstnumber/:number1/secondnumber/:number2/thirdnumber/:number3/fourthnumber/:number4', (req, res) => {
     result=parseInt(req.params.number1)+parseInt(req.params.number2);
