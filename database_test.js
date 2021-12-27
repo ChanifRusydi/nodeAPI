5d7ae42b80915d3af20e11c285cd6bba16952d87
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-27 09:57:34 +0700

    create working on database_test

--- database_test.js
+++ database_test.js
@@ -10,7 +10,24 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'sosial_media'
+    database: 'media_sosial'
 });
-
-
+//sending data
+app.post('/posts', (req, res) => {
+    let sql = 'INSERT INTO posts SET post_date=NOW()'
+    +",username=' "+req.body.username
+    +"', post="+req.body.post_text+"'";
+    // let values = [req.body.user_id, req.body.post_text];
+    db.query(sql, (err, result) => {
+        if (err) throw err; 
+        res.json({ "status": 200, "message": "Post created", "data": null });
+    });
+}); 
+//fetching data
+app.get('/posts', (req, res) => {
+    let sql = 'SELECT post_id, username, post, DATE_FORMAT(post_date, "%d-%m-%Y") AS post_date FROM posts';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json({"status": 200, data: result, message: 'Posts fetched'});
+    });
+});
